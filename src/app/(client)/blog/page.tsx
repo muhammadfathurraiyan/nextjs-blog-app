639efbd238d944ef9ba7473292d92be4193660bf
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-10-11 14:35:17 +0700

    update

--- src/app/(client)/blog/page.tsx
+++ src/app/(client)/blog/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function BlogPage() {
+  const [data, setData] = useState([]);
+
+  const apiCall = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/berry");
+    const data = await response.json();
+    const values = data.results.map(())
+  };
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+
   return (
     <section className="flex min-h-screen items-center justify-center">
       <h1 className="text-3xl">this is blog page</h1>
+      <div>{data}</div>
     </section>
   );
 }
